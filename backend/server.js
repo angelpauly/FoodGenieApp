535d98d60d684fc993508e44f2830a4d0feea85f
Angel Pauly <angelpauly72@gmail.com>
2026-06-28 20:57:01 +0530

    connected db

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 // start the server
 
 const app = require("./app")
+const connectDatabase = require("./config/database")
 
 const dotenv = require("dotenv")
 
 //load env variable
 dotenv.config({path: "./config/config.env"})
+//connec to db
+connectDatabase();
+
 
 //start server
+
+
 app.listen(process.env.PORT, ()=>{
     console.log(`server started on PORT: ${process.env.PORT}`)
 })
